38eafc065411e0f22c15b592594c43479a41a181
Saleh Rahimzadeh <6342081+saleh-rahimzadeh@users.noreply.github.com>
2019-07-18 14:17:07 +0430

    Improving performance by removing directory

--- GoodPrompt.ps1
+++ GoodPrompt.ps1
@@ -1,33 +1,31 @@
 <#----------------------------------------------------------------------------#
  # GoodPrompt                                                                 #
- # Version 1.2                                                                #
+ # Version 1.3                                                                #
  # Copyright (C) Saleh Rahimzadeh                                             #
  # https://github.com/saleh-rahimzadeh/GoodPrompt                             #
  #----------------------------------------------------------------------------#>
 
 function Prompt {
-	<# Preparing details and information of prompt as a data dictionary #>
-	$data = @{
-		Path = '{0} {1}>' -f "`u{2592}",$($ExecutionContext.SessionState.Path.CurrentLocation);
-		Status = '{0}{1} {2} {3} ' -f $(if (test-path variable:/PSDebugContext) { '[DBG] {0} ' -f "`u{2502}" } else { '' }), (get-date -uformat %r), "`u{2502}", $MyInvocation.HistoryId;
-		Bar = "`u{258c}";
-		Cursor = $(if ($host.UI.RawUI.WindowSize.Width -lt $host.UI.RawUI.MaxWindowSize.Width) { "`n" } else { '' }) + "`u{25ba}" * ($NestedPromptLevel + 1);
-	}
+	<# Preparing details and information of prompt #>
+	$Location = '{0} {1}>' -f "`u{2592}",$($ExecutionContext.SessionState.Path.CurrentLocation);
+	$Status = '{0}{1} {2} {3} ' -f $(if (Test-Path variable:/PSDebugContext) { '[DBG] {0} ' -f "`u{2502}" } else { '' }), (get-date -uformat %r), "`u{2502}", $MyInvocation.HistoryId;
+	$Bar = "`u{258c}";
+	$Cursor = $(if ($host.UI.RawUI.WindowSize.Width -lt $host.UI.RawUI.MaxWindowSize.Width) { "`n" } else { '' }) + "`u{25ba}" * ($NestedPromptLevel + 1);
 
 	<# Calculating remained space to draw separator bar #>
-	$space = $host.UI.RawUI.WindowSize.Width - (($data.Path.Length + $data.Bar.Length) % $host.UI.RawUI.WindowSize.Width) - $data.Status.Length
-	if ($space -lt 0) {
-		$space += $host.UI.RawUI.WindowSize.Width
+	$Space = $host.UI.RawUI.WindowSize.Width - (($Location.Length + $Bar.Length) % $host.UI.RawUI.WindowSize.Width) - $Status.Length
+	if ($Space -lt 0) {
+		$Space += $host.UI.RawUI.WindowSize.Width
 		if ($host.UI.RawUI.WindowSize.Width -lt $host.UI.RawUI.MaxWindowSize.Width) { 
-			$space += ($host.UI.RawUI.MaxWindowSize.Width - $host.UI.RawUI.WindowSize.Width)
+			$Space += ($host.UI.RawUI.MaxWindowSize.Width - $host.UI.RawUI.WindowSize.Width)
 		}
 	}
-	$data.Bar += ' ' * $space
+	$Bar += ' ' * $Space
 
 	<# Display prompt #>
-	Write-Host $data.Path   -ForegroundColor Black -BackgroundColor White -NoNewline
-	Write-Host $data.Bar    -ForegroundColor Black -BackgroundColor DarkGray -NoNewLine
-	Write-Host $data.Status -ForegroundColor White -BackgroundColor DarkGray -NoNewLine
-	Write-Host $data.Cursor -ForegroundColor White -NoNewLine
+	Write-Host $Location -ForegroundColor Black -BackgroundColor White -NoNewline
+	Write-Host $Bar      -ForegroundColor Black -BackgroundColor DarkGray -NoNewline
+	Write-Host $Status   -ForegroundColor White -BackgroundColor DarkGray -NoNewline
+	Write-Host $Cursor   -ForegroundColor White -NoNewline
 	return ' '
 }
